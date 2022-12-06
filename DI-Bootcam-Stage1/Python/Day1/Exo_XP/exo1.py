de55510012d67276c80188dbdcfe6411252861c4
Tayga1234 <Korogogaetan1234@gmail.com>
2022-12-06 01:32:46 +0000

    first commit

---
+++ DI-Bootcam-Stage1/Python/Day1/Exo_XP/exo1.py
@@ -0,0 +1,75 @@
+#----------##----Exo1---##----------#
+
+print("Hello World \n"*4)
+
+
+
+#---------####-------Exo2------####---------#
+print("resaultat=",pow(99,3)*8)
+
+
+
+
+####-------Exo3------####
+# 1°) Predisons les resultats
+#a) False
+#b) True
+#c) False
+#d) Erreur
+#d) False
+
+
+
+#------------####-------Exo4------####-----------#
+computer_brand = "HP"
+print("I have a",computer_brand,"computer")
+
+
+
+
+
+#------------####-------Exo5------####-------------#
+
+name="KOROGO Gaetan"
+age=20
+shose_size=43
+info='Je me nomme',name,"j'ai ",age,"ans","j'ai une de ",shose_size,"."
+print(info)
+
+
+
+#----------####-------Exo6------####-----------#
+
+a= 100
+b= 200
+if a>b:
+    print("Hello World")
+    
+    
+#-------------####-------Exo7------####------------#
+
+nb=input("Entrer un nombre:")
+if nb%2==0:
+    print(nb,"est paire")
+else:
+    print(nb,"est impaire")
+    
+    
+    
+#----------####-------Exo8------####------------#
+
+nom0=input("Saisir votre nom: ")
+if nom0=="Gaetan":
+    print("vous avez le meme nom")
+else:
+    print("vos noms sont differents")
+    
+
+
+#-----------####-------Exo9------####------------#
+
+taille=int(input("Entrer votre taille en pouce: "))
+if taille > 145 :
+    print("Vous etes assez grand pour rouler")
+else:
+    print("Vous devez grandir un peu plus pour rouler")
