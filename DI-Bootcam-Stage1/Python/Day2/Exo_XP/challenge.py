4f3ccd9a79af6279ea5c10a8b44ab473ba36e2fa
Tayga1234 <Korogogaetan1234@gmail.com>
2022-12-07 21:50:04 +0000

    ajout

---
+++ DI-Bootcam-Stage1/Python/Day2/Exo_XP/challenge.py
@@ -0,0 +1,15 @@
+######________Challenge 1________######
+
+number=int(input("Entrer un nombre: "))
+length=int(input("Entrer la taille: "))
+L=[]
+for i in range(1,length+1):
+    L.append(number*i)
+    
+print(L)
+
+
+######________Challenge 2________######
+
+ch=input("Entrer une chaine de caractere: ")
+# en cour ...
